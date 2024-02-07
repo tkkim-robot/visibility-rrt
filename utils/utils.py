6c38a8669310e178984f9b0765bd13c507d91985
Taekyung Kim <ktk1501@kakao.com>
2024-02-06 19:49:11 -0500

    add feat: plot the union of FOV

--- utils/utils.py
+++ utils/utils.py
@@ -38,6 +38,40 @@ def calculate_fov_points(position, yaw, fov_angle = math.pi/2, cam_range = 3):
 
     return left_point, right_point
 
+
+def linewidth_from_data_units(linewidth, axis, reference='y'):
+    """
+    Convert a linewidth in data units to linewidth in points.
+
+    Parameters
+    ----------
+    linewidth: float
+        Linewidth in data units of the respective reference-axis
+    axis: matplotlib axis
+        The axis which is used to extract the relevant transformation
+        data (data limits and size must not change afterwards)
+    reference: string
+        The axis that is taken as a reference for the data width.
+        Possible values: 'x' and 'y'. Defaults to 'y'.
+
+    Returns
+    -------
+    linewidth: float
+        Linewidth in points
+    """
+    fig = axis.get_figure()
+    if reference == 'x':
+        length = fig.bbox_inches.width * axis.get_position().width
+        value_range = np.diff(axis.get_xlim())
+    elif reference == 'y':
+        length = fig.bbox_inches.height * axis.get_position().height
+        value_range = np.diff(axis.get_ylim())
+    # Convert length to points
+    length *= 72
+    # Scale linewidth to value range
+    return linewidth * (length / value_range)
+
+
 class Utils:
     def __init__(self):
         self.env = env.Env()
